ecf904a1e90e77d85934a87190dbc4dd3561e650
JD Russo <jdr033@bucknell.edu>
2015-01-25 19:13:48 -0500

    Moved animation code into main block so that it is not executed when the module is imported.

--- schrodinger/schrodinger.py
+++ schrodinger/schrodinger.py
@@ -189,121 +189,123 @@ def theta(x):
 def square_barrier(x, width, height):
     return height * (theta(x) - theta(x - width))
 
-######################################################################
-# Create the animation
 
-# specify time steps and duration
-dt = 0.01
-N_steps = 50
-t_max = 120
-frames = int(t_max / float(N_steps * dt))
+if __name__=='__main__':
+	######################################################################
+	# Create the animation
 
-# specify constants
-hbar = 1.0   # planck's constant
-m = 1.9      # particle mass
+	# specify time steps and duration
+	dt = 0.01
+	N_steps = 50
+	t_max = 120
+	frames = int(t_max / float(N_steps * dt))
 
-# specify range in x coordinate
-N = 2 ** 11
-dx = 0.1
-x = dx * (np.arange(N) - 0.5 * N)
+	# specify constants
+	hbar = 1.0   # planck's constant
+	m = 1.9      # particle mass
 
-# specify potential
-V0 = 1.5
-L = hbar / np.sqrt(2 * m * V0)
-a = 3 * L
-x0 = -60 * L
-V_x = square_barrier(x, a, V0)
-V_x[x < -98] = 1E6
-V_x[x > 98] = 1E6
+	# specify range in x coordinate
+	N = 2 ** 11
+	dx = 0.1
+	x = dx * (np.arange(N) - 0.5 * N)
 
-# specify initial momentum and quantities derived from it
-p0 = np.sqrt(2 * m * 0.2 * V0)
-dp2 = p0 * p0 * 1./80
-d = hbar / np.sqrt(2 * dp2)
+	# specify potential
+	V0 = 1.5
+	L = hbar / np.sqrt(2 * m * V0)
+	a = 3 * L
+	x0 = -60 * L
+	V_x = square_barrier(x, a, V0)
+	V_x[x < -98] = 1E6
+	V_x[x > 98] = 1E6
 
-k0 = p0 / hbar
-v0 = p0 / m
-psi_x0 = gauss_x(x, d, x0, k0)
+	# specify initial momentum and quantities derived from it
+	p0 = np.sqrt(2 * m * 0.2 * V0)
+	dp2 = p0 * p0 * 1./80
+	d = hbar / np.sqrt(2 * dp2)
 
-# define the Schrodinger object which performs the calculations
-S = Schrodinger(x=x,
-                psi_x0=psi_x0,
-                V_x=V_x,
-                hbar=hbar,
-                m=m,
-                k0=-28)
+	k0 = p0 / hbar
+	v0 = p0 / m
+	psi_x0 = gauss_x(x, d, x0, k0)
 
-######################################################################
-# Set up plot
-fig = pl.figure()
+	# define the Schrodinger object which performs the calculations
+	S = Schrodinger(x=x,
+	                psi_x0=psi_x0,
+	                V_x=V_x,
+	                hbar=hbar,
+	                m=m,
+	                k0=-28)
 
-# plotting limits
-xlim = (-100, 100)
-klim = (-5, 5)
+	######################################################################
+	# Set up plot
+	fig = pl.figure()
 
-# top axes show the x-space data
-ymin = 0
-ymax = V0
-ax1 = fig.add_subplot(211, xlim=xlim,
-                      ylim=(ymin - 0.2 * (ymax - ymin),
-                            ymax + 0.2 * (ymax - ymin)))
-psi_x_line, = ax1.plot([], [], c='r', label=r'$|\psi(x)|$')
-V_x_line, = ax1.plot([], [], c='k', label=r'$V(x)$')
-center_line = ax1.axvline(0, c='k', ls=':',
-                          label = r"$x_0 + v_0t$")
+	# plotting limits
+	xlim = (-100, 100)
+	klim = (-5, 5)
 
-title = ax1.set_title("")
-ax1.legend(prop=dict(size=12))
-ax1.set_xlabel('$x$')
-ax1.set_ylabel(r'$|\psi(x)|$')
+	# top axes show the x-space data
+	ymin = 0
+	ymax = V0
+	ax1 = fig.add_subplot(211, xlim=xlim,
+	                      ylim=(ymin - 0.2 * (ymax - ymin),
+	                            ymax + 0.2 * (ymax - ymin)))
+	psi_x_line, = ax1.plot([], [], c='r', label=r'$|\psi(x)|$')
+	V_x_line, = ax1.plot([], [], c='k', label=r'$V(x)$')
+	center_line = ax1.axvline(0, c='k', ls=':',
+	                          label = r"$x_0 + v_0t$")
 
-# bottom axes show the k-space data
-ymin = abs(S.psi_k).min()
-ymax = abs(S.psi_k).max()
-ax2 = fig.add_subplot(212, xlim=klim,
-                      ylim=(ymin - 0.2 * (ymax - ymin),
-                            ymax + 0.2 * (ymax - ymin)))
-psi_k_line, = ax2.plot([], [], c='r', label=r'$|\psi(k)|$')
+	title = ax1.set_title("")
+	ax1.legend(prop=dict(size=12))
+	ax1.set_xlabel('$x$')
+	ax1.set_ylabel(r'$|\psi(x)|$')
 
-p0_line1 = ax2.axvline(-p0 / hbar, c='k', ls=':', label=r'$\pm p_0$')
-p0_line2 = ax2.axvline(p0 / hbar, c='k', ls=':')
-mV_line = ax2.axvline(np.sqrt(2 * V0) / hbar, c='k', ls='--',
-                      label=r'$\sqrt{2mV_0}$')
-ax2.legend(prop=dict(size=12))
-ax2.set_xlabel('$k$')
-ax2.set_ylabel(r'$|\psi(k)|$')
+	# bottom axes show the k-space data
+	ymin = abs(S.psi_k).min()
+	ymax = abs(S.psi_k).max()
+	ax2 = fig.add_subplot(212, xlim=klim,
+	                      ylim=(ymin - 0.2 * (ymax - ymin),
+	                            ymax + 0.2 * (ymax - ymin)))
+	psi_k_line, = ax2.plot([], [], c='r', label=r'$|\psi(k)|$')
 
-V_x_line.set_data(S.x, S.V_x)
+	p0_line1 = ax2.axvline(-p0 / hbar, c='k', ls=':', label=r'$\pm p_0$')
+	p0_line2 = ax2.axvline(p0 / hbar, c='k', ls=':')
+	mV_line = ax2.axvline(np.sqrt(2 * V0) / hbar, c='k', ls='--',
+	                      label=r'$\sqrt{2mV_0}$')
+	ax2.legend(prop=dict(size=12))
+	ax2.set_xlabel('$k$')
+	ax2.set_ylabel(r'$|\psi(k)|$')
 
-######################################################################
-# Animate plot
-def init():
-    psi_x_line.set_data([], [])
-    V_x_line.set_data([], [])
-    center_line.set_data([], [])
+	V_x_line.set_data(S.x, S.V_x)
 
-    psi_k_line.set_data([], [])
-    title.set_text("")
-    return (psi_x_line, V_x_line, center_line, psi_k_line, title)
+	######################################################################
+	# Animate plot
+	def init():
+	    psi_x_line.set_data([], [])
+	    V_x_line.set_data([], [])
+	    center_line.set_data([], [])
 
-def animate(i):
-    S.time_step(dt, N_steps)
-    psi_x_line.set_data(S.x, 4 * abs(S.psi_x))
-    V_x_line.set_data(S.x, S.V_x)
-    center_line.set_data(2 * [x0 + S.t * p0 / m], [0, 1])
+	    psi_k_line.set_data([], [])
+	    title.set_text("")
+	    return (psi_x_line, V_x_line, center_line, psi_k_line, title)
 
-    psi_k_line.set_data(S.k, abs(S.psi_k))
-    title.set_text("t = %.2f" % S.t)
-    return (psi_x_line, V_x_line, center_line, psi_k_line, title)
+	def animate(i):
+	    S.time_step(dt, N_steps)
+	    psi_x_line.set_data(S.x, 4 * abs(S.psi_x))
+	    V_x_line.set_data(S.x, S.V_x)
+	    center_line.set_data(2 * [x0 + S.t * p0 / m], [0, 1])
 
-# call the animator.  blit=True means only re-draw the parts that have changed.
-anim = animation.FuncAnimation(fig, animate, init_func=init,
-                               frames=frames, interval=30, blit=True)
+	    psi_k_line.set_data(S.k, abs(S.psi_k))
+	    title.set_text("t = %.2f" % S.t)
+	    return (psi_x_line, V_x_line, center_line, psi_k_line, title)
+
+	# call the animator.  blit=True means only re-draw the parts that have changed.
+	anim = animation.FuncAnimation(fig, animate, init_func=init,
+	                               frames=frames, interval=30, blit=True)
 
 
-# uncomment the following line to save the video in mp4 format.  This
-# requires either mencoder or ffmpeg to be installed on your system
+	# uncomment the following line to save the video in mp4 format.  This
+	# requires either mencoder or ffmpeg to be installed on your system
 
-#anim.save('schrodinger_barrier.mp4', fps=15, extra_args=['-vcodec', 'libx264'])
+	#anim.save('schrodinger_barrier.mp4', fps=15, extra_args=['-vcodec', 'libx264'])
 
-pl.show()
+	pl.show()
